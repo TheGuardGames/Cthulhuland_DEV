1df64f527188753e115f23b157910178fdb0be5a
lothvorn <lothvorn@gmail.com>
2015-12-13 00:51:47 +0100

    working with interaction
    Skeleton ready at 90%

--- Assets/TheGuardAssets/Scripts/Interaction/InteractionManager.cs
+++ Assets/TheGuardAssets/Scripts/Interaction/InteractionManager.cs
@@ -3,13 +3,10 @@ using System.Collections;
 
 public class InteractionManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	//keep public
+	public InteractionAgent currentlyPointedAgent = null;
+
+
+
+
 }
--- Assets/TheGuardAssets/Scripts/Interaction/InteractorTool.cs
+++ Assets/TheGuardAssets/Scripts/Interaction/InteractorTool.cs
@@ -7,21 +7,27 @@ public class InteractorTool : MonoBehaviour {
 	public float triggerTime = 3f;
 	
 	//set private
+	///for inner processes
 	public float triggerTimer = 0;
 	public InteractionAgent currentAgent;
 	public InteractionAgent lastSelected;
 	public GameObject player;
 	public Camera playerCamera;
 	//public WaypointManager waypointManager;
-	public Animator animator;
+	//public Animator animator;
 	//public WaypointBehaviour currentWaypoint;
-	
+	/// references to external
+	public InteractionManager interactionManager;
+
 	// Use this for initialization
 	void Start () {
+		//initialize inner processes vars
 		player = GameObject.FindGameObjectWithTag ("MainCamera").gameObject;
 		playerCamera = player.GetComponent<Camera>();
 		//waypointManager = GameObject.FindGameObjectWithTag ("WaypointHelpers").GetComponent<WaypointManager>();
-		animator = GetComponent<Animator>();
+		//animator = GetComponent<Animator>();
+		interactionManager = GameObject.FindGameObjectWithTag ("GameController").GetComponent<InteractionManager>();
+		Debug.Log (interactionManager.name);
 	}
 	
 	// Update is called once per frame
@@ -38,10 +44,13 @@ public class InteractorTool : MonoBehaviour {
 		Physics.Raycast (playerCamera.transform.position, playerCamera.transform.forward, out hit, interactionDistance);
 		
 		if (hit.transform != null && hit.transform.tag == "InteractiveByRay"){
+			interactionManager.currentlyPointedAgent = hit.transform.GetComponent<InteractionAgent>();
 			//Debug.Log (hit.transform.name);
-			currentAgent = hit.transform.GetComponent<InteractionAgent>();
+			//currentAgent = hit.transform.GetComponent<InteractionAgent>();  //esto sobra, ahora esta en interaction manager
+
 		} else{
-			currentAgent = null;
+			interactionManager.currentlyPointedAgent = null;
+			//currentAgent = null;//esto sobra, ahora esta en interaction manager
 		}
 		
 		
